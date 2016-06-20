04cb429e1f4c9c1b92162ce8e0e03d8f12f271c5
Bob Posey <bobposey@gmail.com>
2016-06-20 16:55:22 -0700

    quick test to see if a more unique ID will resolve the local notification crash

--- scripts/index.js
+++ scripts/index.js
@@ -245,9 +245,9 @@ registerPermission = function () {
 scheduleDelayed = function () {
     var now = new Date().getTime(),
         _5_sec_from_now = new Date(now + 5 * 1000);
-    var sound = device.platform == 'Android' ? 'file://sound.mp3' : 'file://beep.caf';
+    // var sound = device.platform == 'Android' ? 'file://sound.mp3' : 'file://beep.caf';
     ons.notification.alert('trying to set notification now...');
-    window.plugins.notification.local.schedule({
+    cordova.plugins.notification.local.schedule({
         id: 17,
         title: 'Scheduled with delay',
         message: 'Test Message 1',
